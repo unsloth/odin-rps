d07c5d1202f03f855d4040e86f74dc0166ab63e7
Michael Huang <michaelh@casual.place>
2022-10-07 18:50:40 -0700

    Add function to compare choices

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.trim().toLowerCase();
+    computerSelection = computerSelection.trim().toLowerCase();
+
+    console.log(playerSelection);
+    console.log(computerSelection);
+
+    switch(true) {
+        case playerSelection === computerSelection: 
+            return "Tied!";
+            break;
+        case (playerSelection === "rock" && computerSelection === "scissors"):
+        case (playerSelection === "paper" && computerSelection === "rock"):
+        case (playerSelection === "scissors" && computerSelection === "paper"):
+            return "Round Won!";
+            break;
+        default: 
+            return "Round Lost!";
+    }
+}
+
+console.log(playRound("rock", getComputerChoice()));
