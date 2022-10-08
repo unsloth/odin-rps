e3f1321194b0a6ff58166058b9b1f72d95da1c0b
Michael Huang <michaelh@casual.place>
2022-10-07 20:17:10 -0700

    Make separate player choice function
    Separated getting a player choice from the playRound function.
    Included input check and formatting in new function to manage
    unaccounted inputs. Changed getComputerChoice function to return
    properly formatted strings, removing the need for formatting in
    playRound function and making it align with getPlayerChoice.

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 10);
     if (rand < 3) {
-        return "Rock";
+        return "rock";
     } else if (rand < 6) {
-        return "Paper";
-    } else return "Scissors";
+        return "paper";
+    } else return "scissors";
+}
+
+function getPlayerChoice() {
+    
+    let noAnswer = true;
+    let choice = "";
+    while (noAnswer) {
+        choice = prompt("Play Rock Paper Scissors:", "rock");
+        choice = choice.trim().toLowerCase();
+        noAnswer = false;
+
+        if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+            console.log("Not an option, try again");
+            noAnswer = true;
+        }
+    }
+    return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
-
-    playerSelection = playerSelection.trim().toLowerCase();
-    computerSelection = computerSelection.trim().toLowerCase();
 
     console.log(playerSelection);
     console.log(computerSelection);
@@ -34,7 +48,7 @@ function game() {
     let score = 0;
 
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Play Rock Paper Scissors:", "rock");
+        let playerSelection = getPlayerChoice();
         let computerSelection = getComputerChoice();
 
         let result = playRound(playerSelection, computerSelection);
